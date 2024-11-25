05d37a472229e0fe573c93bdcd349b0270b300ae
lalman lama <lalman.creamcode@gmail.com>
2024-11-25 13:18:53 +0545

    Doccumentation Added.
    -Doccumentation added .

--- pages/kb/wishlist/_meta.ts
+++ pages/kb/wishlist/_meta.ts
@@ -1,5 +1,11 @@
 export default {
 	installation: "Installation",
 	settings: "Settings",
+	general: "General options",
+	"popup-modal": "Popup Modals",
+	"wishlist-button": "Wishlist Button",
+	"wishlist-page": "Wishlist Page",
+	"wishlist-sidebar": "Wishlist Sidebar",
+	"wishlist-notice": "Wishlist Notice",
 	"troubleshooting-cache": "Troubleshooting cache",
 };
