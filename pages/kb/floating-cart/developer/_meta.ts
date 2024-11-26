0b58f1b5fa599fc1b8fd1c79199cde19a3ef59c2
Anuj Subedi <struggleage@gmail.com>
2024-11-26 11:38:54 +0545

    :package: Updated: floating cart docs.
    - :package: Updated: floating cart docs.

---
+++ pages/kb/floating-cart/developer/_meta.ts
@@ -0,0 +1,8 @@
+export default {
+	hooks: "Hooks",
+	"template-overwriting": "Template overwriting",
+	"javascript-api": "JavaScript API",
+	shortcode: "Shortcode",
+	"disable-overlay": "Disable overlay",
+	changelog: "Changelog",
+};
