2406c9187635b4337914a5211341f010afa41ce7
lalman lama <lalman.creamcode@gmail.com>
2024-11-28 17:59:25 +0545

    Wishlist doccumentation updated.
    -Wishlist doccumentation updated.

--- pages/kb/wishlist/developer/_meta.ts
+++ pages/kb/wishlist/developer/_meta.ts
@@ -2,6 +2,6 @@ export default {
 	hooks: "Hooks",
 	functions: "Functions",
 	"custom-button": "Custom button",
-	changelog: "Changelog",
 	"template-overwriting": "Template overwriting",
+	changelog: "Changelog",
 };
